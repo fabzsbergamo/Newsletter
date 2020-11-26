a3857d1e02b4cee628dbc00922367a60c639fe98
Fabio Sbergamo <43303280+fabzsbergamo@users.noreply.github.com>
2020-11-26 16:10:50 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MY_API_KEY : 'e2014be47d6d39d45fc6e32cbea5ddf5-us7',
+    LIST_ID : '16017e0180',
+  };
+
+// API KEY - e2014be47d6d39d45fc6e32cbea5ddf5-us7
+// List ID (aka audience ID) - 16017e0180 . You get this ID from mailchimp. It will help you identify the list you want your subcribers to go into.
